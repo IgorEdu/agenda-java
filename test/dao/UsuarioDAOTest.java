39d380be4394e56fc582dfc77adb30b82720d140
Igor <igoreduardo20101@gmail.com>
2024-06-11 16:57:35 -0300

    test: adiciona teste de cadastro de usuario em banco de dados

--- test/dao/UsuarioDAOTest.java
+++ test/dao/UsuarioDAOTest.java
@@ -1,5 +1,42 @@
 package dao;
 
-public class UsuarioDAOTest {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+import entities.Usuario;
+
+public class UsuarioDAOTest {
+	
+	public static void cadastrarUsuarioDAOTest() throws SQLException, IOException, ParseException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Usuario usuario = new Usuario();
+		usuario.setUsername("Usuario");
+		usuario.setSenha("senhaforte123");
+		usuario.setNomeUsuario("Novo Usuario");
+		usuario.setDataNascimento(new java.sql.Date(sdf.parse("19/02/1999").getTime()));
+		usuario.setGenero("MASCULINO");
+		usuario.setEmail("emailfulano@email.com");
+		
+
+		Connection conn = BancoDados.conectar();
+		new UsuarioDAO(conn).cadastrar(usuario);
+
+		System.out.println("Cadastro efetuado com sucesso.");
+	}
+	
+	public static void main(String[] args) {
+
+		try {
+			UsuarioDAOTest.cadastrarUsuarioDAOTest();
+		} catch (SQLException | IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
 }
