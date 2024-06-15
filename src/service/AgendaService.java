2a78822ded8982ab27244b6b3683ae7129a043ca
Igor <igoreduardo20101@gmail.com>
2024-06-14 21:40:53 -0300

    feat: adiciona metodo cadastrar agenda

--- src/service/AgendaService.java
+++ src/service/AgendaService.java
@@ -15,10 +15,11 @@ public class AgendaService {
 		this.agendas = new ArrayList<Agenda>();
 		
 		//int idAgenda, Usuario usuario, String nomeAgenda, String descricao, List<Compromisso> compromissos
-		agendas.add(new Agenda(0, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda1", "agenda 1", null));
-		agendas.add(new Agenda(1, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda2", "agenda 2", null));
-		agendas.add(new Agenda(2, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda3", "agenda 3", null));
-		agendas.add(new Agenda(3, new Usuario("joaozinho", "bambam", "Joaozinho Santos", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "joaozinho@outlook.com", "joao.png"), "Agenda4", "agenda 4", null));
+		cadastrarAgenda(new Agenda(0, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda1", "agenda 1", null));
+		cadastrarAgenda(new Agenda(1, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda2", "agenda 2", null));
+		cadastrarAgenda(new Agenda(2, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda3", "agenda 3", null));
+		cadastrarAgenda(new Agenda(3, new Usuario("joaozinho", "bambam", "Joaozinho Santos", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "joaozinho@outlook.com", "joao.png"), "Agenda4", "agenda 4", null));
+	
 	}
 	
 	public List<Agenda> pesquisarAgendasUsuario(Usuario usuario){
@@ -33,5 +34,9 @@ public class AgendaService {
 		}
 		
 		return resultado;
+	}
+	
+	public void cadastrarAgenda(Agenda agenda) {
+		this.agendas.add(agenda);
 	}
 }
