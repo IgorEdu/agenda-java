c01b96ca17f81ad4cdd52fa43d495c1a92b159ea
Igor <igoreduardo20101@gmail.com>
2024-06-18 21:37:29 -0300

    feat: integra compromisso service com classe dao

--- src/service/CompromissoService.java
+++ src/service/CompromissoService.java
@@ -1,115 +1,80 @@
 package service;
 
-//<<<<<<< HEAD
-//import java.io.IOException;
-//import java.sql.Connection;
-//import java.sql.SQLException;
-//
-//import dao.BancoDados;
-//import dao.CompromissoAgendaDAO;
-//import dao.CompromissoDAO;
-//import entities.Agenda;
-//import entities.Compromisso;
-//
-//public class CompromissoService {
-//	public CompromissoService() {
-//		
-//	}
-//	
-//	public void cadastrar(Compromisso compromisso, Agenda agenda) throws SQLException {
-//		try {
-//			Connection conn = BancoDados.conectar();
-//			new CompromissoDAO(conn).cadastrar(compromisso);
-//			
-//			Connection conn2 = BancoDados.conectar();
-//			compromisso = new CompromissoDAO(conn2).buscarCompromissoSemId(compromisso);
-//			
-//			Connection conn3 = BancoDados.conectar();
-//			new CompromissoAgendaDAO(conn3).cadastrar(compromisso.getIdCompromisso(), agenda.getIdAgenda());
-//			
-//		} catch (SQLException | IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//	}
-//	
-//	public void atualizar(Compromisso compromisso) throws SQLException {
-//		try {
-//			Connection conn = BancoDados.conectar();
-//			new CompromissoDAO(conn).atualizar(compromisso);
-//		} catch (SQLException | IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public void excluir(Compromisso compromisso) throws SQLException {
-//		try {
-//			Connection conn = BancoDados.conectar();
-//			new CompromissoDAO(conn).excluir(compromisso);
-//		} catch (SQLException | IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//=======
-import java.sql.Date;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import dao.BancoDados;
+import dao.CompromissoAgendaDAO;
+import dao.CompromissoDAO;
+import entities.Agenda;
 import entities.Compromisso;
 
 public class CompromissoService {
-
-	List<Compromisso> compromissos;
 	
 	public CompromissoService() {
-		
-		this.compromissos = new ArrayList<Compromisso>();
-		
-		Compromisso comp1 = new Compromisso(1, "Comp 1", "Compromisso 1", Date.valueOf("2024-06-30"),"22:22",Date.valueOf("2024-06-30"),"23:00", "UTFPR",Date.valueOf("2024-06-30"),"22:00");
-		this.compromissos.add(comp1);
 	}
 	
-	public List<Compromisso> buscarCompromissosAgenda(int idAgenda){
+	public void cadastrar(Compromisso compromisso, Agenda agenda) throws SQLException {
+	try {
+		Connection conn = BancoDados.conectar();
+		new CompromissoDAO(conn).cadastrar(compromisso);
 		
-		//O ID vai ser usado só quando integrar com o DAO, pq os compromissos são guardados dentro da agenda
+		Connection conn2 = BancoDados.conectar();
+		compromisso = new CompromissoDAO(conn2).buscarCompromissoSemId(compromisso);
+		
+		Connection conn3 = BancoDados.conectar();
+		new CompromissoAgendaDAO(conn3).cadastrar(compromisso.getIdCompromisso(), agenda.getIdAgenda());
+		
+	} catch (SQLException | IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+}
+	
+	public List<Compromisso> buscarCompromissosAgenda(int idAgenda) throws SQLException, IOException{
+		Connection conn = BancoDados.conectar();
+		CompromissoAgendaDAO compromissoAgendaDAO = new CompromissoAgendaDAO(conn);
+		
+		List<Integer> listaIdCompromissos = new ArrayList<Integer>();
+		listaIdCompromissos = compromissoAgendaDAO.listarIdCompromissosPorAgenda(idAgenda);
 		
 		List<Compromisso> resultado = new ArrayList<Compromisso>();
 		
-		for(Compromisso c : compromissos) {
-			
-			resultado.add(c);
+		
+		for(Integer idCompromisso : listaIdCompromissos) {
+			Connection conn2 = BancoDados.conectar();
+			resultado.add(new CompromissoDAO(conn2).buscarCompromissoPorIdCompromisso(idCompromisso));
 		}
 		
 		return resultado.isEmpty() ? null : resultado;
 	}
 	
-	public int atualizarCompromisso(int idCompromisso, Compromisso novo) {
-		
-		for(Compromisso c : compromissos) {
-			
-			if(c.getIdCompromisso() == idCompromisso) {
-				
-				compromissos.add(novo);
-				compromissos.remove(c);
-				return 1;
-			}
+	public int atualizarCompromisso(Compromisso compromisso) throws SQLException {
+		try {
+			Connection conn = BancoDados.conectar();
+			new CompromissoDAO(conn).atualizar(compromisso);
+			return 1;
+		} catch (SQLException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		return 0;
-//>>>>>>> 3bc438fbffbf85992b4b316887c2ee9472e368d7
 	}
+
 	
-	public int excluirCompromisso(int idCompromisso) {
-		
-		for(Compromisso c : compromissos) {
-			
-			if(c.getIdCompromisso() == idCompromisso) {
-				
-				compromissos.remove(c);
-				return 1;
-			}
+	public int excluirCompromisso(int idCompromisso) throws SQLException {
+		try {
+			Connection conn = BancoDados.conectar();
+			new CompromissoDAO(conn).excluir(idCompromisso);
+			return 1;
+		} catch (SQLException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		return 0;
