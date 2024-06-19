89a988ba66ce60b839101da15ff93b4d44338edc
Igor <igoreduardo20101@gmail.com>
2024-06-19 15:26:16 -0300

    test: testa funcionalidades de compromissoService

--- test/service/CompromissoServiceTest.java
+++ test/service/CompromissoServiceTest.java
@@ -1,59 +1,114 @@
 package service;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import entities.Agenda;
+import entities.Compromisso;
 import entities.Usuario;
 
 public class CompromissoServiceTest {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void cadastrarTest(Compromisso compromisso, Agenda agenda) throws SQLException {
 		CompromissoService compromissoService = new CompromissoService();
 		
-		UsuarioService userService = new UsuarioService();
-		Usuario usuario = new Usuario("igorzinho", "bumbum", "Igor Eduardo", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "igor@gmail.com", "igor.png");
+		compromissoService.cadastrar(compromisso, agenda);
+	}
+	
+	public static List<Compromisso> buscarCompromissosAgendaTest(int idAgenda) throws SQLException, IOException{
+		CompromissoService compromissoService = new CompromissoService();
 		
-		if(userService.cadastrar(usuario)) {
-			System.out.println("Usuario cadastrado com sucesso!");
+		List<Compromisso> resultado = new ArrayList<Compromisso>();
+		resultado = compromissoService.buscarCompromissosAgenda(idAgenda);
+		
+		return resultado.isEmpty() ? null : resultado;
+	}
+	
+	public static void atualizarCompromissoTest(Compromisso compromisso) throws SQLException {
+		CompromissoService compromissoService = new CompromissoService();
+		
+		try {
+			int retorno =  compromissoService.atualizarCompromisso(compromisso);
+			
+			if(retorno == 1) {
+				System.out.println("Compromisso: " + compromisso.getIdCompromisso() + " atualizado!");
+			} else {
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+	}
+	
+	public static void excluirCompromissoTest(int idCompromisso) {
+		CompromissoService compromissoService = new CompromissoService();
+		
+		try {
+			int retorno =  compromissoService.excluirCompromisso(idCompromisso);
+			
+			if(retorno == 1) {
+				System.out.println("Compromisso: " + idCompromisso + " excluido!");
+			} else {
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void importarArquivoCSVTest(String path, Agenda agenda) {
+		CompromissoService compromissoService = new CompromissoService();
+		
+		compromissoService.importarArquivoCSV(path, agenda);
+	}
+	
+	public static void exportarArquivoCSV(String path, Agenda agenda) {
+		CompromissoService compromissoService = new CompromissoService();
+		
+		compromissoService.exportarArquivoCSV(path, agenda);
+	}
+	
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			UsuarioService usuarioService = new UsuarioService();
+			AgendaService agendaService = new AgendaService();
+			
+			Usuario usuario = usuarioService.buscarUsuarioPorLogin("novo");
+			
+			List<Agenda> agendas = agendaService.buscarAgendasUsuario(usuario);
+			Agenda agenda1 = agendas.get(0);
+			
+			Agenda agenda2 = agendas.get(1);
 
-		
-		userService.atualizarUsuario(usuario);
-		
-		Usuario usuarioBusca = userService.buscarUsuarioPorLogin(usuario.getUsername());
-		System.out.println();
-		System.out.println("=====================================================");
-		System.out.println("Usuário teste de busca:");
-		System.out.println("Id: " + usuarioBusca.getId());
-		System.out.println("Login: " + usuarioBusca.getUsername());
-		System.out.println("Senha (criptografada): "+ usuarioBusca.getSenhaCriptografada());
-		System.out.println("Nome: " + usuarioBusca.getNomeUsuario());
-		System.out.println("Data de nascimento: " + usuarioBusca.getDataNascimento());
-		System.out.println("Gênero: " + usuarioBusca.getGenero());
-		System.out.println("E-mail: " + usuarioBusca.getEmail());
-		System.out.println("======================================================");
-		System.out.println();
-		
-		List<Usuario> outrosUsuarios = new ArrayList<Usuario>();
-		//outrosUsuarios = userService.listarOutrosUsuario(usuario);
-		
-		for(Usuario outroUsuario : outrosUsuarios) {
-			System.out.println();
-			System.out.println("=====================================================");
-			System.out.println("Usuário teste de busca da listagem:");
-			System.out.println("Id: " + outroUsuario.getId());
-			System.out.println("Login: " + outroUsuario.getUsername());
-			System.out.println("Senha (criptografada): "+ outroUsuario.getSenhaCriptografada());
-			System.out.println("Nome: " + outroUsuario.getNomeUsuario());
-			System.out.println("Data de nascimento: " + outroUsuario.getDataNascimento());
-			System.out.println("Gênero: " + outroUsuario.getGenero());
-			System.out.println("E-mail: " + outroUsuario.getEmail());
-			System.out.println("======================================================");
-			System.out.println();
+			
+			Compromisso compromisso = new Compromisso();
+			compromisso.setTitulo("Teste service");
+			compromisso.setDescricao("Compromisso de teste na service");
+			compromisso.setDataInicio(new java.sql.Date(sdf.parse("01/01/2024").getTime()));
+			compromisso.setHorarioInicio("10:01:30");
+			compromisso.setDataTermino(new java.sql.Date(sdf.parse("01/01/2024").getTime()));
+			compromisso.setHorarioTermino("10:50:30");
+			compromisso.setLocal("Sala");
+			
+			
+			cadastrarTest(compromisso, agenda1);
+			
+			exportarArquivoCSV("./assets", agenda1);
+			
+			importarArquivoCSVTest("./assets/compromissos-idagenda-11.csv", agenda2);
+		} catch (ParseException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		userService.excluirUsuario(usuario);
 		
 	}
 }
