c53c80410b0811d3147433228a4c0d8c95c42a76
Igor <igoreduardo20101@gmail.com>
2024-06-19 15:25:51 -0300

    feat: adiciona importacao e exportacao de CSV

--- src/service/CompromissoService.java
+++ src/service/CompromissoService.java
@@ -1,8 +1,15 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +20,7 @@ import entities.Agenda;
 import entities.Compromisso;
 
 public class CompromissoService {
+	private static final String SEPARADOR_CSV = ";";
 	
 	public CompromissoService() {
 	}
@@ -78,5 +86,122 @@ public class CompromissoService {
 		}
 		
 		return 0;
+	}
+
+	public void importarArquivoCSV(String path, Agenda agenda) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			line = br.readLine();
+			while (line != null) {
+				String[] vetor = line.split(SEPARADOR_CSV);
+				
+				
+				Compromisso compromisso = new Compromisso();
+				compromisso.setTitulo(vetor[0]);
+				compromisso.setDescricao(vetor[1]);
+				
+				if(!String.valueOf(vetor[2]).isEmpty() && !String.valueOf(vetor[2]).equals("null")) {
+					compromisso.setDataInicio(new java.sql.Date(sdf.parse(vetor[2]).getTime()));
+				}
+				
+				compromisso.setHorarioInicio(vetor[3]);
+				
+				if(!String.valueOf(vetor[4]).isEmpty() && !String.valueOf(vetor[4]).equals("null")) {
+					compromisso.setDataTermino(new java.sql.Date(sdf.parse(vetor[4]).getTime()));
+				}
+				
+				if(!String.valueOf(vetor[8]).isEmpty() && !String.valueOf(vetor[8]).equals("null")) {
+					compromisso.setHorarioNotificacao(vetor[8]);
+				}
+				
+				compromisso.setHorarioTermino(vetor[5]);
+				compromisso.setLocal(vetor[6]);
+				
+				if(!String.valueOf(vetor[7]).isEmpty() && !String.valueOf(vetor[7]).equals("null")) {
+					compromisso.setDataNotificacao(new java.sql.Date(sdf.parse(vetor[7]).getTime()));
+				}
+				
+				if(!String.valueOf(vetor[8]).isEmpty() && !String.valueOf(vetor[8]).equals("null")) {
+					compromisso.setHorarioNotificacao(vetor[8]);
+				}
+					
+				System.out.println();
+				System.out.println("==============================");
+				System.out.println(compromisso.getTitulo());
+				System.out.println(compromisso.getDescricao());
+				System.out.println(compromisso.getDataInicio());
+				System.out.println(compromisso.getHorarioInicio());
+				System.out.println(compromisso.getDataTermino());
+				System.out.println(compromisso.getHorarioTermino());
+				System.out.println(compromisso.getLocal());
+				System.out.println(compromisso.getDataNotificacao());
+				System.out.println(compromisso.getHorarioNotificacao());
+				System.out.println("==============================");
+				
+				cadastrar(compromisso, agenda);
+				
+				line = br.readLine();
+				
+			}
+		}
+		 catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void exportarArquivoCSV(String path, Agenda agenda) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		String idAgenda = String.valueOf(agenda.getIdAgenda());
+		String nomeArquivo = path.concat("/compromissos-idagenda-").concat(idAgenda).concat(".csv");
+		
+		List<Compromisso> compromissos = new ArrayList<Compromisso>();
+//		boolean arquivoExiste = new File(path).exists();
+//		if(new File(path))
+		try {
+			FileWriter writer = new FileWriter(nomeArquivo, StandardCharsets.ISO_8859_1);
+			
+			writer.write("Titulo" + SEPARADOR_CSV + "Descricao" + SEPARADOR_CSV + "DataInicio" + SEPARADOR_CSV 
+					+ "HorarioInicio"+ SEPARADOR_CSV + "DataTermino" + SEPARADOR_CSV + "HorarioTermino" + SEPARADOR_CSV 
+					+ "Local" + SEPARADOR_CSV + "DataNotificacao" + SEPARADOR_CSV + "HorarioNotificacao\n");
+			
+			compromissos = buscarCompromissosAgenda(agenda.getIdAgenda());
+			
+			for(Compromisso compromisso : compromissos) {
+				String dataInicioFormatada;
+				String dataTerminoFormatada;
+				String dataNotificacaoFormatada;
+				
+				dataInicioFormatada = (String.valueOf(compromisso.getDataInicio()).isEmpty() || String.valueOf(compromisso.getDataInicio()).equals("null")) ? null : String.valueOf(sdf.format(compromisso.getDataInicio()));
+				dataTerminoFormatada = (String.valueOf(compromisso.getDataTermino()).isEmpty() || String.valueOf(compromisso.getDataTermino()).equals("null")) ? null : String.valueOf(sdf.format(compromisso.getDataTermino()));
+				dataNotificacaoFormatada = (String.valueOf(compromisso.getDataNotificacao()).isEmpty() || String.valueOf(compromisso.getDataNotificacao()).equals("null")) ? null : String.valueOf(sdf.format(compromisso.getDataNotificacao()));
+				
+				writer.write(compromisso.getTitulo() + SEPARADOR_CSV + compromisso.getDescricao() 
+					+ SEPARADOR_CSV + dataInicioFormatada + SEPARADOR_CSV + compromisso.getHorarioInicio() 
+					+ SEPARADOR_CSV + dataTerminoFormatada + SEPARADOR_CSV + compromisso.getHorarioTermino() + SEPARADOR_CSV + compromisso.getLocal() 
+					+ SEPARADOR_CSV + dataNotificacaoFormatada + SEPARADOR_CSV + compromisso.getHorarioNotificacao() + "\n"
+				);
+			}
+			
+			writer.flush();
+			writer.close();
+			
+		} catch (IOException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
