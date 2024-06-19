4343a83c7184b3c0672ac221a520585f9ecf61ec
Igor <igoreduardo20101@gmail.com>
2024-06-18 21:03:54 -0300

    fix: corrige tabela usuario inclui caminho da imagem

--- dump-db/dump-agenda-utfpr.sql
+++ dump-db/dump-agenda-utfpr.sql
@@ -30,7 +30,7 @@ CREATE TABLE `agendas` (
   PRIMARY KEY (`id`),
   KEY `agendas_usuarios_FK` (`id_usuario`),
   CONSTRAINT `agendas_usuarios_FK` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,7 @@ CREATE TABLE `agendas` (
 
 LOCK TABLES `agendas` WRITE;
 /*!40000 ALTER TABLE `agendas` DISABLE KEYS */;
-INSERT INTO `agendas` VALUES (2,4,'Agenda de teste','Descrição da agenda usada para teste do método'),(3,4,'Agenda de teste','Descrição da agenda usada para teste do método'),(4,4,'Agenda de teste','Descrição da agenda usada para teste do método'),(5,4,'Agenda de teste','Descrição da agenda usada para teste do método');
+INSERT INTO `agendas` VALUES (2,4,'Agenda de teste','Descrição da agenda usada para teste do método'),(3,4,'Agenda de teste','Descrição da agenda usada para teste do método'),(4,4,'Agenda de teste','Descrição da agenda usada para teste do método'),(5,4,'Agenda de teste','Descrição da agenda usada para teste do método'),(11,7,'nova','agenda nova');
 /*!40000 ALTER TABLE `agendas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,7 +62,7 @@ CREATE TABLE `compromissos` (
   `data_notificacao` date DEFAULT NULL,
   `horario_notificacao` time DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,8 +71,36 @@ CREATE TABLE `compromissos` (
 
 LOCK TABLES `compromissos` WRITE;
 /*!40000 ALTER TABLE `compromissos` DISABLE KEYS */;
-INSERT INTO `compromissos` VALUES (3,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(4,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(5,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL);
+INSERT INTO `compromissos` VALUES (3,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','13:30:59','2024-05-05','12:30:00','Sala de reuniões',NULL,NULL),(4,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(5,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(6,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(7,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(8,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(9,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(10,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(11,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(12,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(13,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL),(14,'Compromisso teste','Compromisso usado para testar a classe DAO','2024-05-05','12:30:59','2024-05-05','11:30:00','Sala de reuniões',NULL,NULL);
 /*!40000 ALTER TABLE `compromissos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compromissos_agendas`
+--
+
+DROP TABLE IF EXISTS `compromissos_agendas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compromissos_agendas` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_compromissos` bigint unsigned NOT NULL,
+  `id_agendas` bigint unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `compromissos_agendas_compromissos_FK` (`id_compromissos`),
+  KEY `compromissos_agendas_agendas_FK` (`id_agendas`),
+  CONSTRAINT `compromissos_agendas_agendas_FK` FOREIGN KEY (`id_agendas`) REFERENCES `agendas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `compromissos_agendas_compromissos_FK` FOREIGN KEY (`id_compromissos`) REFERENCES `compromissos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compromissos_agendas`
+--
+
+LOCK TABLES `compromissos_agendas` WRITE;
+/*!40000 ALTER TABLE `compromissos_agendas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compromissos_agendas` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -119,9 +147,10 @@ CREATE TABLE `usuarios` (
   `nascimento` date NOT NULL,
   `genero` varchar(20) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `caminho_imagem` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,7 +159,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (4,'teste 1','agsasrqwfqwwqwv','usuario teste','2024-06-13','MASCULINO','emailteste@email.com');
+INSERT INTO `usuarios` VALUES (4,'teste 1','agsasrqwfqwwqwv','usuario teste','2024-06-13','MASCULINO','emailteste@email.com',NULL),(7,'novo','A665A45920422F9D417E4867EFDC4FB8A04A1F3FFF1FA07E998E86F7F7A27AE3','novo usuario','1999-01-01','Masculino','novo@novo.com',NULL),(8,'igorzinho','A48E7CEB4415311590CA6BA9C2A5F1D337489CCE1DC6D38B2E4B12A6AF5D827C','Igor Eduardo','2024-06-14','Masculino','igor@gmail.com',NULL);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,4 +176,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-17 20:37:13
+-- Dump completed on 2024-06-18 21:02:05
