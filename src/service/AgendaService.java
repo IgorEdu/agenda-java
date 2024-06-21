0cbdc1aca9d8623df656bc07b82927014a8c328f
Igor <igoreduardo20101@gmail.com>
2024-06-20 21:00:40 -0300

    cleanup: elimina blocos de código comentado

--- src/service/AgendaService.java
+++ src/service/AgendaService.java
@@ -12,25 +12,12 @@ import entities.Agenda;
 import entities.Usuario;
 
 public class AgendaService {
-
-//	private List<Agenda> agendas;
-//	private AgendaDAO agendaDAO;
 	
 	public AgendaService() {
-		
-//		this.agendas = new ArrayList<Agenda>();
-//		
-//		//int idAgenda, Usuario usuario, String nomeAgenda, String descricao, List<Compromisso> compromissos
-//		cadastrarAgenda(new Agenda(0, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda1", "agenda 1", null));
-//		cadastrarAgenda(new Agenda(1, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda2", "agenda 2", null));
-//		cadastrarAgenda(new Agenda(2, new Usuario("murilinho", "123", "Murilo Vozniaki", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "murilo@gmail.com", "murilo.png"), "Agenda3", "agenda 3", null));
-//		cadastrarAgenda(new Agenda(3, new Usuario("joaozinho", "bambam", "Joaozinho Santos", new java.sql.Date(Long.valueOf("1718384913992")), "Masculino", "joaozinho@outlook.com", "joao.png"), "Agenda4", "agenda 4", null));
-	
+
 	}
 	
 	public void cadastrarAgenda(Agenda agenda) {
-		
-//		this.agendas.add(agenda);
 		try {
 			Connection conn = BancoDados.conectar();
 			AgendaDAO agendaDAO = new AgendaDAO(conn);
@@ -50,11 +37,6 @@ public class AgendaService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-//		for(Agenda agenda : agendas) {
-//			
-//			if(agenda.getIdAgenda() == idAgenda) return agenda;
-//		}
 		return null;
 	}
 	
@@ -65,12 +47,6 @@ public class AgendaService {
 			Connection conn = BancoDados.conectar();
 			AgendaDAO agendaDAO = new AgendaDAO(conn);
 			resultado = agendaDAO.buscarAgendaPorIdUsuario(usuario.getId());
-//			for(Agenda agenda : agendas) {
-//				
-//				if(agenda.getUsuario().getUsername().equalsIgnoreCase(usuario.getUsername())) {
-//					resultado.add(agenda);
-//				}
-//			}
 		} catch (SQLException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -90,16 +66,6 @@ public class AgendaService {
 			e.printStackTrace();
 		}
 		
-//		for(Agenda agenda : agendas) {
-//			
-//			if(agenda.getIdAgenda() == agendaNova.getIdAgenda()) {
-//				
-//				agendas.add(agendaNova);
-//				agendas.remove(agenda);
-//				return 1;
-//			}
-//		}
-		
 		return 0;
 	}
 	
@@ -114,14 +80,5 @@ public class AgendaService {
 			e.printStackTrace();
 		}
 		return 0;
-//		for(Agenda agenda : agendas) {
-//			
-//			if(agenda.getIdAgenda() == idAgenda) {
-//				
-//				agendas.remove(agenda);
-//				return 1;
-//			}
-//		}
-//		return 0;
 	}
 }
